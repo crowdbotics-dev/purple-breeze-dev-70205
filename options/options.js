73d1a690b6ea9030b4fb655a5bda7f5446ea3b34
Crowdbotics <engineering@crowdbotics.com>
2023-03-17 10:29:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-breeze-dev-70205",
+  url: "https://purple-breeze-dev-70205.botics.co",
+  api: "https://purple-breeze-dev-70205.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
